8ad1e36b020635b397adf19bf17a291896e5bc9a
Finn <153606679+flyrora@users.noreply.github.com>
2024-12-06 22:20:04 +0100

    Update pc-info.bat

--- pc-info.bat
+++ pc-info.bat
@@ -5,32 +5,48 @@ echo ======================================
 echo      PC-Informationen by Flyrora 
 echo ======================================
 
+REM
 echo Computername: %COMPUTERNAME%
 echo Benutzername: %USERNAME%
 echo.
 
+REM
 echo Betriebssystemdetails:
 systeminfo | findstr /B /C:"Betriebssystemname" /C:"Betriebssystemversion"
 echo.
 
+REM
 echo Prozessor und Arbeitsspeicher:
 wmic cpu get name
 wmic memorychip get capacity
 echo.
 
+REM
 echo Festplatteninformationen:
 wmic logicaldisk get name,filesystem,freespace,size
 echo.
 
+REM
 echo IP-Adressen:
 ipconfig | findstr /C:"IPv4" /C:"IPv6"
 echo.
 
+REM
 echo Netzwerkadapterstatus:
 wmic nic get name,netconnectionstatus | findstr /C:"Connected" /C:"Verbunden"
 echo.
 
-Ping anzeigen
+REM
+echo Betriebszeit des PCs:
+for /f "tokens=1-4 delims=," %%a in ('net statistics workstation ^| find "since"') do echo %%a %%b %%c %%d
+echo.
+
+REM
+echo Akkustand:
+wmic path win32_battery get estimatedchargeremaining || echo Kein Akku gefunden oder Desktop-PC.
+echo.
+
+REM
 echo Aktueller Ping:
 ping 8.8.8.8 -n 1 | findstr "Antwort" || echo Internet nicht erreichbar.
 echo.
